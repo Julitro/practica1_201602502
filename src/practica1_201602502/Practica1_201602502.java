0b5906f9fc868c5ccd569699cf454bb2b3a2b6d6
Usuario <Usuario@DESKTOP-CFT0RVT>
2017-08-24 21:30:52 -0600

    proyectoterminado.

---
+++ src/practica1_201602502/Practica1_201602502.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1_201602502;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica1_201602502 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new parctica_201602502 ();  
+        // TODO code application logic here
+    }
+    
+}
